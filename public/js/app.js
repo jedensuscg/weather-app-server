aa33987b3ef180c3164f61bce8dac4444041c859
James Edens <jedensuscg@gmail.com>
2020-12-14 15:11:58 -0500

    Added inital search form and basic console logging.

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,24 @@
-console.log('Client Side Javascript loaded')
+console.log('Client Side Javascript loaded');
+
+
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const location = search.value;
+
+    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+        response.json().then((data) => {
+            if (data.errorMsg) {
+                console.log(`ERROR: ${data.errorMsg}`);
+            } else {
+                console.log(data.geocode.Location)
+                console.log(`Current Temp is: ${data.currentForecast.temp}`);
+            }
+        });
+    });
+})
