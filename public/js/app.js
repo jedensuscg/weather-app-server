b65e1ad71be599cf5c750c04a76a3b2c71d2f9d0
James Edens <jedensuscg@gmail.com>
2021-01-28 11:26:13 -0500

    changes

--- public/js/app.js
+++ public/js/app.js
@@ -12,34 +12,34 @@ console.log("Client Side Javascript loaded");
 
 // #region DECLARATIONS
 
-const weatherContent = document.querySelector("#weather-content");
-const forecastWeatherContent = document.querySelector("#forecast-weather-content");
-const dailyForecastContent = document.querySelector('#forecast-daily-content');
+const weatherContentDiv = document.querySelector("#weather-content");
+const forecastWeatherContentDiv = document.querySelector("#forecast-weather-content");
+const dailyForecastContentDiv = document.querySelector('#forecast-daily-content');
 const weatherForm = document.querySelector("form");
-const search = document.querySelector("input");
-const weatherHead = document.querySelector("#weather-head");
+const searchInput = document.querySelector("input");
+const weatherHeaderP = document.querySelector("#weather-head");
 const currentTemp = document.querySelector("#current-temp");
 const feelsLike = document.querySelector("#feels-like");
 const currentWinds = document.querySelector("#current-winds");
-const precip = document.querySelector("#precip");
-const humidity = document.querySelector("#humidity");
+const currentPrecipitation = document.querySelector("#precip");
+const currentHumidity = document.querySelector("#humidity");
 const weatherCode = document.querySelector("#weather-code");
 const forecastPrecipChance = document.querySelector("#forecast-precip-chance");
 const forecastWeatherHeader = document.querySelector("#forecast-weather-head");
 const betweenForecastHR = document.createElement("hr");
-const flatIconAtt = document.querySelector(".attribution");
+const flatIconAttributeImg = document.querySelector(".attribution");
 const climacellIcon = document.querySelector(".climacell-attribute");
-const currentLeftDIv = document.querySelector(".current-left-div");
-const currentRightDiv = document.querySelector(".current-right-div");
+const currentForecastLeftDIv = document.querySelector(".current-left-div");
+const currentForecastRightDiv = document.querySelector(".current-right-div");
 const hourlyTabButton = document.querySelector('#hourly-tab-button');
 const dailyTabButton = document.querySelector('#daily-tab-button');
 let firstSearch = true;
 let hourListDiv = undefined;
-flatIconAtt.style.visibility = "hidden";
+flatIconAttributeImg.style.visibility = "hidden";
 climacellIcon.style.visibility = "hidden";
 dailyTabButton.style.visibility = 'hidden';
 hourlyTabButton.style.visibility = 'hidden';
-forecastWeatherContent.style.visibility = 'hidden';
+forecastWeatherContentDiv.style.visibility = 'hidden';
 const month = [
   "January",
   "February",
@@ -68,19 +68,20 @@ dailyTabButton.addEventListener('click', function(){
 weatherForm.addEventListener("submit", (e) => {
   e.preventDefault();
   console.log("running fetch");
-  weatherContent.style.background = "inherit";
-  const location = search.value;
+  weatherContentDiv.style.background = "inherit";
+  const location = searchInput.value;
 
   clearPreviousSearch();
-
   firstSearch = false;
+
   fetch(`/weather?address=${location}`).then((response) => {
     response.json().then((data) => {
       const { geocode, currentForecast, futureForecast, dailyForecast } = data;
-      weatherContent.classList.add("weather-border");
+      
+      weatherContentDiv.classList.add("weather-border");
 
       if (data.errorMsg) {
-        weatherHead.textContent = "ERROR";
+        weatherHeaderP.textContent = "ERROR";
         currentTemp.textContent = `${data.errorMsg}`;
         currentTemp.classList.add("error");
       } else {
@@ -89,39 +90,17 @@ weatherForm.addEventListener("submit", (e) => {
         displayFiveDayWeather(dailyForecast);
 
 
-        flatIconAtt.style.visibility = "visible";
+        flatIconAttributeImg.style.visibility = "visible";
         climacellIcon.style.visibility = "visible";
-        weatherContent.style.background = "white";
-        weatherHead.style.color = "black";
-        forecastWeatherContent.style.visibility = 'visible';
+        weatherContentDiv.style.background = "white";
+        weatherHeaderP.style.color = "black";
+        forecastWeatherContentDiv.style.visibility = 'visible';
         dailyTabButton.style.visibility = 'visible';
         hourlyTabButton.style.visibility = 'visible';
       }
     });
   });
 });
-
-/**
- * @memberof ClientSide
- * @description Capitalize the first word of a string.
- * @param {string} string String to capitalize first word.
- * @return {string} Capitalized string.
- */
-function capitalize(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-}
-
-/**
- * @memberof ClientSide
- * @description Removes underscores and capitalizes first word of string.
- * @param {string} string String to normalize
- * @example "hot_and_sunny" becomes "Hot and sunny"
- * @return {string} Normalized and capitalized string.
- */
-function normalizeString(string) {
-  str = string.replace("_", " ");
-  return str.charAt(0).toUpperCase() + str.slice(1);
-}
 
 /**
  * @memberof ClientSide
@@ -132,7 +111,7 @@ function normalizeString(string) {
  * @see {@link module:currentForecast} for how current forecast data is retrieved.
  */
 function displayCurrentWeather(geocode, currentForecast) {
-  weatherHead.textContent = `Your Current Weather for ${geocode.Location}`;
+  weatherHeaderP.textContent = `Your Current Weather for ${geocode.Location}`;
 
   const currentConditionIcon = createWeatherIcon(
     `/img/${currentForecast.weatherCode}.svg`,
@@ -155,24 +134,24 @@ function displayCurrentWeather(geocode, currentForecast) {
     currentForecast.units
   }`;
 
-  weatherCode.textContent = `Conditions: ${normalizeString(
+  weatherCode.textContent = `Conditions: ${capitalizeString(normalizeString(
     currentForecast.weatherCode
-  )}`;
+  ))}`;
   feelsLike.textContent = `Feels Like: ${Math.ceil(currentForecast.feelsLike)}${
     currentForecast.units
   }`;
   currentWinds.textContent = `Winds: ${
     currentForecast.cardinalWindHeading
   } at ${Math.ceil(currentForecast.windSpeed)} ${currentForecast.windUnits}`;
-  humidity.textContent = `Humidity: ${currentForecast.humidity}%`;
+  currentHumidity.textContent = `Humidity: ${currentForecast.humidity}%`;
   if (currentForecast.precipitationType != null) {
-    precip.textContent = `Precipitation: ${capitalize(
+    currentPrecipitation.textContent = `Precipitation: ${capitalizeString(
       currentForecast.precipWord
-    )} ${capitalize(currentForecast.precipitationType)} (${
+    )} ${capitalizeString(currentForecast.precipitationType)} (${
       currentForecast.precipitation
     } ${currentForecast.precipitationUnits})`;
   } else {
-    precip.textContent = `Precipitation: There is currently no precipitation`;
+    currentPrecipitation.textContent = `Precipitation: There is currently no precipitation`;
   }
 }
 
@@ -183,7 +162,7 @@ function displayCurrentWeather(geocode, currentForecast) {
  * @see {@link module:futureForecast} for how current forecast data is retrieved.
  */
 function displayForecastWeather(futureForecast) {
-  forecastWeatherContent.insertBefore(betweenForecastHR, forecastWeatherHeader);
+  forecastWeatherContentDiv.insertBefore(betweenForecastHR, forecastWeatherHeader);
   hourListDiv = createForecastList(futureForecast);
   forecastPrecipChance.before(hourListDiv);
 
@@ -197,17 +176,23 @@ function displayForecastWeather(futureForecast) {
 
 function displayFiveDayWeather(dailyForecast) {
   dailyList = createDailyList(dailyForecast);
-  dailyForecastContent.appendChild(dailyList)
+  dailyForecastContentDiv.appendChild(dailyList)
 }
 
 function createDailyList({dailyWeather: days}) {
+  // Create the list of days (div) to hold weather information
   const dayList = createElementWithClass('div', 'daily-forecast-list')
   let day = 0;
+
+  // Find when the forecast date matches current local date, in case API returns previous dates due to UTC.
+  // Sets day to equal this index.
   for (let index = 0; index < days.length; index++) {
     date = new Date(days[index].time)
     if (date.getDate() == new Date().getDate())
     day = index
   }
+
+  // Iterates over each day and creates the associated weather code Icon (i.e rain, sunny, etc.)
   for (day; day < days.length; day++) {
     const dayData = days[day];
     const conditionIcon = createWeatherIcon(
@@ -216,37 +201,46 @@ function createDailyList({dailyWeather: days}) {
       "small-condition-icon",
       "condition icon"
     );
+
+    // Create rest of the HTML Elements
+
     const dayDiv = createElementWithClass('div', 'forecast-daily-div')
     const leftDiv = createElementWithClass('div', 'day-left-div');
     const rightDiv = createElementWithClass('div', 'day-right-div');
     const dateH3 = createElementWithClass('h3', 'forecast-day-date');
-    
-    // Temps
     const lowTempDiv = createElementWithClass('div', 'daily-low-temp-div')
     const highTempDiv = createElementWithClass('div', 'daily-high-temp-div')
+    const rainChanceDiv = createElementWithClass('div', 'daily-rain-chance-div')
+
+    // FIll out Temps
     lowTempDiv.appendChild(document.createTextNode(`LOW: ${dayData.minTemp}`))
     highTempDiv.appendChild(document.createTextNode(`HIGH: ${dayData.maxTemp}`))
-    leftDiv.appendChild(lowTempDiv);
-    leftDiv.appendChild(highTempDiv);
-
-    //Rain Chance
-    const rainChanceDiv = createElementWithClass('div', 'daily-rain-chance-div')
+  
+    //FIll out Rain Chance
     rainChanceDiv.appendChild(document.createTextNode(`Precipitation: ${dayData.rainChance}`))
-    leftDiv.appendChild(rainChanceDiv)
-
-
+    
+    // Append Date Header
     dateH3.appendChild(document.createTextNode(getDayAndMonth(dayData.time)))
     dayDiv.appendChild(dateH3)
+
+    // Append left Div
+    leftDiv.appendChild(lowTempDiv);
+    leftDiv.appendChild(highTempDiv);
+    leftDiv.appendChild(rainChanceDiv)
+
+    // Append right Div
     rightDiv.appendChild(conditionIcon)
-    
+
+    // Append everything to the day Div
     dayDiv.appendChild(leftDiv)
     dayDiv.appendChild(rightDiv)
+
+    // Append to the list of days Div.
     dayList.appendChild(dayDiv)
   }
 
   return dayList;
 }
-
 
 /**
  * @memberof ClientSide
@@ -256,49 +250,50 @@ function createDailyList({dailyWeather: days}) {
  * @return {HTMLDivElement} The container DIV with all individual child divs for each hour.
  */
 function createForecastList({ hourWeather: hours }) {
+  // Create the list of hours (Div) to display weather data.
+  const hourList = createElementWithClass("div", "forecast-hourly-div");
+  hourList.id = "hour-list";
+  
   let printedNewDay = false;
-  let nightDay = "";
+  let nightOrDay = "";
+  
   const lineBreak = document.createElement("br");
-  const tempImg = createWeatherIcon(
+
+  // create Icons with desired dimensions
+  const temperatureIcon = createWeatherIcon(
     "/img/temperature.svg",
     "25",
     "small-temp-icon",
     "temp gauge"
   );
-  const rainDropImg = createWeatherIcon(
+  const raidDropIcon = createWeatherIcon(
     "/img/drop.svg",
     "15",
     "small-precip-icon",
     "rain drop"
   );
 
-  const hourList = document.createElement("div");
-  hourList.className = "forecast-hourly-div";
-  hourList.id = "hour-list";
-
+  // Iterate through each hour and create element and fill with data.
   for (let hour = 1; hour < hours.length; hour++) {
     const hourData = hours[hour];
+
+    // Create weather icon for specified weather code (ie Sunny, rain, etc.)
     const conditionIcon = createWeatherIcon(
       `/img/${hourData.weatherCode}.svg`,
       "60",
       "small-condition-icon",
       "condition icon"
     );
+
+    // Create HTML Elements
     const leftDiv = createElementWithClass("div", "hour-left-div");
     const rightDiv = createElementWithClass("div", "hour-right-div");
     const timeH4 = createElementWithClass("h4", "forecast-hour-time");
     const tempP = createElementWithClass("span", "forecast-hour-temp");
     const rainP = createElementWithClass("span", "forecast-hour-rain");
-    if (hourData.dayNight == "day") {
-      nightDay = "forecast-hour-div-day";
-
-    } else {
-      nightDay = "forecast-hour-div-night";
-      timeH4.style.color = 'white'
-    }
-    const hourDiv = createElementWithClass("div", `${nightDay}`);
+    const hourDiv = createElementWithClass("div", `${ assignDayOrNightClass(hourData, timeH4)}`);
     const conditionDiv = createElementWithClass("div", "small-condition-div");
-
+    const dateSpan = createElementWithClass("span", "date-span");
     const time = readableFormatLocalTIme(hourData.time);
     const observationDate = new Date(hourData.time);
     const observationDay = observationDate.getDate();
@@ -308,38 +303,48 @@ function createForecastList({ hourWeather: hours }) {
     hourDiv.appendChild(leftDiv);
     hourDiv.appendChild(rightDiv);
 
+    // Append TODAY to dateSpan first hour time, and TOMORROW to Midnight hour dateSpan
     timeH4.appendChild(document.createTextNode(time));
     if (hour == 1) {
-      const dateSpan = createElementWithClass("span", "date-span");
       const dateString = `TODAY`;
       dateSpan.appendChild(document.createTextNode(dateString));
       dateSpan.style.color = 'black'
-      timeH4.appendChild(dateSpan);
     }
     if ((observationDay > currentDate.getDate()) & (printedNewDay == false)) {
-      const dateSpan = createElementWithClass("span", "date-span");
-
       const dateString = `TOMORROW`;
       dateSpan.appendChild(document.createTextNode(dateString));
-      timeH4.appendChild(dateSpan);
       printedNewDay = true;
     }
 
+    // Append elements to DOM
+    timeH4.appendChild(dateSpan);
     tempP.appendChild(document.createTextNode(hourData.temp));
     rainP.appendChild(document.createTextNode(hourData.rainChanceAtHour));
-
-    leftDiv.appendChild(tempImg.cloneNode(true));
+    leftDiv.appendChild(temperatureIcon.cloneNode(true));
     leftDiv.appendChild(tempP);
     leftDiv.appendChild(lineBreak.cloneNode(true));
-    leftDiv.appendChild(rainDropImg.cloneNode(true));
+    leftDiv.appendChild(raidDropIcon.cloneNode(true));
     leftDiv.appendChild(rainP);
-
     rightDiv.appendChild(conditionDiv);
     conditionDiv.appendChild(conditionIcon);
 
+    // Append current hour to hourList
     hourList.appendChild(hourDiv);
   }
   return hourList;
+
+  //Object Functions
+  function assignDayOrNightClass(hourData, timeH4) {
+    if (hourData.dayNight == "day") {
+      nightOrDay = "forecast-hour-div-day";
+
+    } else {
+      nightOrDay = "forecast-hour-div-night";
+      timeH4.style.color = 'white';
+    }
+    return nightOrDay;
+  }
+
 }
 
 
@@ -365,7 +370,7 @@ function createWeatherIcon(src, dimensions, CSSclass, alt) {
 /**
  * @memberof ClientSide
  * @description Removes all child nodes of the forecast weather div, and then removes the parent.
- * Ran each time new search is requested.
+ * Ran each time new searchInput is requested.
  * @param {string} parent Parent of children to remove from DOM
  *
  */
@@ -377,23 +382,23 @@ function removeAllChildNodes(parent) {
 
 /**
  * @memberof ClientSide
- * @description Clears all stale weather data from webpage when new search is performed.
+ * @description Clears all stale weather data from webpage when new searchInput is performed.
  */
 function clearPreviousSearch() {
-  if (weatherHead.textContent != "ERROR") {
-    weatherHead.textContent = "Loading Weather";
-    weatherHead.style.color = "white";
+  if (weatherHeaderP.textContent != "ERROR") {
+    weatherHeaderP.textContent = "Loading Weather";
+    weatherHeaderP.style.color = "white";
     currentTemp.textContent = "";
     currentWinds.textContent = "";
-    precip.textContent = "";
+    currentPrecipitation.textContent = "";
     feelsLike.textContent = "";
-    humidity.textContent = "";
+    currentHumidity.textContent = "";
     weatherCode.textContent = "";
     forecastPrecipChance.textContent = "";
     betweenForecastHR.remove();
     climacellIcon.style.visibility = "hidden";
-    flatIconAtt.style.visibility = "hidden";
-    forecastWeatherContent.style.visibility = 'hidden';
+    flatIconAttributeImg.style.visibility = "hidden";
+    forecastWeatherContentDiv.style.visibility = 'hidden';
     dailyTabButton.style.visibility = 'hidden';
     hourlyTabButton.style.visibility = 'hidden';
 
@@ -406,7 +411,7 @@ function clearPreviousSearch() {
       largeTempIcon.remove();
     }
   } else {
-    weatherHead.textContent = "Loading Weather";
+    weatherHeaderP.textContent = "Loading Weather";
     currentTemp.textContent = "";
     currentTemp.classList.remove("error");
   }
@@ -438,10 +443,30 @@ function getDayAndMonth(dateTime){
       
       return returnString
     }
-
-
   }
 
+}
+
+/**
+ * @memberof ClientSide
+ * @description Capitalize the first word of a string.
+ * @param {string} string String to capitalize first word.
+ * @return {string} Capitalized string.
+ */
+function capitalizeString(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+/**
+ * @memberof ClientSide
+ * @description Removes underscores from string.
+ * @param {string} string String to normalize
+ * @example "hot_and_sunny" becomes "Hot and sunny"
+ * @return {string} Normalized and capitalized string.
+ */
+function normalizeString(string) {
+  str = string.replace("_", " ");
+  return str;
 }
 
 function readableFormatLocalTIme(dateTime) {
